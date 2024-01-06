f18ea1af8bd2e154efd3399963cbaf9e3bc739f0
haider0107 <kushal.git01@gmail.com>
2024-01-07 04:04:48 +0530

    user and article model added

---
+++ backend/src/models/article.model.js
@@ -0,0 +1,32 @@
+import mongoose, { Schema } from "mongoose";
+
+const articleSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    featureImage: {
+      type: String,
+      default: "",
+    },
+    status: {
+      type: String,
+      index: true,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Article = mongoose.model("Article", articleSchema);
