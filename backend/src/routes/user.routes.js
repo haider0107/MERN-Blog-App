39e62320ea76c1234eb2bd4c71be4f4d8bc96d00
haider0107 <kushal.git01@gmail.com>
2024-01-08 00:39:29 +0530

    User Route added

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,8 +1,37 @@
 import { Router } from "express";
 import { upload } from "../middlewares/multer.middleware.js";
+import {
+  changeCurrentPassword,
+  getCurrentUser,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  registerUser,
+  updateAccountDetails,
+} from "../controllers/user.controllers.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
+// Register User route
+router.route("/register").post(registerUser);
 
+// Login User route
+router.route("/login").post(loginUser);
+
+// logout user
+router.route("/logout").post(verifyJWT, logoutUser);
+
+// Refresh token route
+router.route("/refresh-token").post(refreshAccessToken);
+
+// Change password route
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+
+// Get current user route
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+
+// Update user details
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
 
 export default router;
