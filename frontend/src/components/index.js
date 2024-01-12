7f73712af97bbe71d9495f823e694aff4e99af5c
haider0107 <kushal.git01@gmail.com>
2024-01-13 00:53:59 +0530

    Login and signup components added , article card and auth layout added, select component added

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -3,5 +3,6 @@ import Footer from "./Footer/Footer";
 import LogoutBtn from "./Header/LogoutBtn";
 import Logo from "./Logo";
 import Container from "./container/Container";
+import Input from "./Input";
 
-export { Header, Footer, LogoutBtn, Logo, Container };
+export { Header, Footer, LogoutBtn, Logo, Container, Input };
