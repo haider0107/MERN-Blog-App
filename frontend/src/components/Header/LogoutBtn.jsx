720eb13cbe3f0b0ddecef0fdaba13eaa8fc828d9
haider0107 <kushal.git01@gmail.com>
2024-01-12 19:08:54 +0530

    Footer and Header components added, Tailwind setup

---
+++ frontend/src/components/Header/LogoutBtn.jsx
@@ -0,0 +1,24 @@
+import { useDispatch } from "react-redux";
+import { logoutUser } from "../../Server/auth";
+import { logout } from "../../store/authSlice";
+
+function LogoutBtn() {
+  const dispatch = useDispatch();
+
+  const logoutHandler = () => {
+    logoutUser().then(() => {
+      dispatch(logout());
+    });
+  };
+
+  return (
+    <button
+      className="inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+      onClick={logoutHandler}
+    >
+      Logout
+    </button>
+  );
+}
+
+export default LogoutBtn;
